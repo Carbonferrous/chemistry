322bb3aedfff0aa523c132520b4bd5706fa10fe7
carbonferrous <carbonferrous@gmail.com>
2016-09-26 19:02:47 -0500

    schrodinger ploting

---
+++ invtransamp.py
@@ -0,0 +1,16 @@
+import math, random
+#root find for cdf in range 0<x<1 CDF must be monotomicaly increasing
+#basically a binary search for solution between [0, 1]
+def rootfind(u, cdf, epsilon = .0001):
+    x = .5
+    i = 1
+    while abs(cdf(x)-u)>epsilon:
+        if cdf(x)<u:
+            x += 1/2**i
+        else:
+            x -= 1/2**i
+        i += 1
+    return x
+#returns a random number between [0, 1) which has the distribution
+def distrand(cdf):
+    return rootfind(random.random(), cdf)
---
+++ schrodinger.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import scipy, math
+import invtransamp as its
+
+#Schrodinger cummulative distribution
+def SCDF(x, n):
+    return x - math.sin(2*n*math.pi*x)/(2*n*math.pi)
+
+def sbox(nx, ny, nz, n = 1000):
+    fig = plt.figure()
+    ax = ax = fig.add_subplot(111, projection='3d')
+
+    x = np.array(list(its.distrand(lambda i: SCDF(i, nx)) for c in range(n)))
+    y = np.array(list(its.distrand(lambda i: SCDF(i, ny)) for c in range(n)))
+    z = np.array(list(its.distrand(lambda i: SCDF(i, nz)) for c in range(n)))
+
+    ax.scatter(x, y, z, c='r', marker='s')
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    plt.show()
+
+sbox(2, 2, 2)
